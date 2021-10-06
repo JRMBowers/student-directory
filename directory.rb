d81218dce45a57eef6de350377d1f02e12cb8dec
Jamie Bowers <jamiebowers3@gmail.com>
2021-10-06 12:48:58 +0100

    directory.rb added

---
+++ directory.rb
@@ -0,0 +1,11 @@
+student_count = 8
+puts "Ant Man"
+puts "Captain America"
+puts "Spider Man"
+puts "Black Panther"
+puts "Thor"
+puts "Doctor Strange"
+puts "Iron Man"
+puts "Hulk"
+
+puts "We have #{student_count} great students"
