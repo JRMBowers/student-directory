551e24b6363625ed2465f3e27108173b7791d503
Jamie Bowers <jamiebowers3@gmail.com>
2021-10-06 14:26:23 +0100

    Code broke down into functions

--- directory.rb
+++ directory.rb
@@ -3,7 +3,19 @@ students = ["Ant Man", "Captain America", "Spider Man",
    "Black Panther", "Thor", "Doctor Strange", "Iron Man", "Hulk"]
 student_count = students.count
 
-puts "The students of Marvel Academy"
-puts "------------------------------"
-students.each {|student| puts student}
-puts "Overall, we have #{student_count} students"
+def print_header
+  puts "The students of Marvel Academy"
+  puts "------------------------------"
+end
+
+def print_names(students)
+  students.each {|student| puts student}
+end
+
+def print_footer(student_count)
+  puts "Overall, we have #{student_count} students"
+end 
+
+print_header
+print_names(students)
+print_footer(student_count)
